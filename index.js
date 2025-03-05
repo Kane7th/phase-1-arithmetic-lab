2fb24ee5b5d0d5d2820063d8afaf1c0f59a03ed3
Kane Kabena <onekaneldn@gmail.com>
2025-03-05 10:53:30 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+// Multiply
+const num1 = 2 
+const num2 = 31
+
+const multiply = (num1) * (num2) 
+
+// Random
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+// Mod 
+const num3 = 10
+const num4 = 6
+
+const mod = (num3) % (num4)
+console.log(mod)
+
+const max = Math.max(1, 5, 7, 14, 20);
+console.log(max)
